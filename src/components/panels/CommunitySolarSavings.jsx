71e4d8111750e6f251ea8b668c3b568f4b291f22
Dusty211 <kylebenz@hotmail.com>
2019-08-22 23:27:30 -0400

    Added icons and placed in savings panel. Needs work with formatting and sizing.

--- src/components/panels/CommunitySolarSavings.jsx
+++ src/components/panels/CommunitySolarSavings.jsx
@@ -7,6 +7,10 @@ import {
   CardTitle
 } from "reactstrap";
 
+import { ReactComponent as SunIcon } from '../../assets/img/savesolar/sun-solid.svg'
+import { ReactComponent as LinkIcon } from '../../assets/img/savesolar/link-solid.svg'
+import { ReactComponent as DollarIcon } from '../../assets/img/savesolar/dollar-sign-solid.svg'
+
 class CommunitySolarSavings extends React.Component {
 
   render() {
@@ -19,24 +23,24 @@ class CommunitySolarSavings extends React.Component {
           </CardHeader>
           <CardBody>
           <Alert color="primary" style={{padding: 8, display: 'flex', alignItems: 'center'}} >
-            <div className="d-inline-block bg-warning" style={{maxWidth: '20%'}} >
-              <span>Icon goes here</span>
+            <div className="d-inline-block bg-white" style={{width: '100px', height: '100px'}} >
+              <SunIcon color="#0746a5" title='Sun icon'/>
             </div>
-            <div className="d-inline-block" style={{maxWidth: '80%', paddingLeft: 10}} >
+            <div className="d-inline-block" style={{ paddingLeft: 10}} >
               <span>Your Community Solar facility generated 22,500 kWh for the last billing cycle</span>
             </div>
           </Alert>
           <Alert color="primary" style={{padding: 8, display: 'flex', alignItems: 'center'}} >
-            <div className="d-inline-block bg-warning" style={{maxWidth: '20%'}} >
-              <span>Icon goes here</span>
+            <div className="d-inline-block bg-white" style={{padding: 15, width: 100, height: 100}} >
+              <LinkIcon color="#0746a5" title='Link icon'/>
             </div>
             <div className="d-inline-block" style={{maxWidth: '80%', paddingLeft: 10}} >
               <span>Your 5% allocation equates to 750 kWh’s over the period</span>
             </div>
           </Alert>
           <Alert color="primary" style={{padding: 8, display: 'flex', alignItems: 'center'}} >
-            <div className="d-inline-block bg-warning" style={{maxWidth: '20%'}} >
-              <span>Icon goes here</span>
+            <div className="d-inline-block bg-white" style={{padding: 28, width: 100, height: 100}} >
+              <DollarIcon color="#0746a5" title='Dollar icon'/>
             </div>
             <div className="d-inline-block" style={{maxWidth: '80%', paddingLeft: 10}} >
               <span>Your SaveSolar 10% discount equates to $21.50 in savings this statement!</span>
